5a3a6d9e544760136782eb0c0e25ebc7a62f5602
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-07-18 14:12:10 +0200

    Adds verbose message to exception that is not self-descriptive

--- R/check.R
+++ R/check.R
@@ -151,8 +151,7 @@ solve_ip.min_isolated_deps_installation_proposal <- function(ip) { # nolint
   })
 
   new_res <- do.call(rbind, deps_res)
-
-  new_res <- new_res[!duplicated(new_res[, c("ref", "package", "version")]), ]
+  new_res <- new_res[!duplicated(new_res[, c("ref", "package", "version", "parent")]), ]
 
   # Keep res at top
   new_res <- rbind(res[1, ], new_res)
