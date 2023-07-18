31ff4481bacb92b539884101b63bf5f794b19810
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-07-18 17:09:50 +0200

    text: Improves on message

--- R/get_ref.R
+++ R/get_ref.R
@@ -100,7 +100,12 @@ get_ref_min.remote_ref_cran <- function(remote_ref, op = "", op_ver = "") {
     pkgdepends::parse_pkg_ref(new_ref),
     error = function(err) {
       cli::cli_alert_danger(
-        "Possible problem finding release for: `{remote_ref$package} ({op} {op_ver})`"
+        paste(
+          sep = " ",
+          "Possible problem finding release for:",
+          "`{remote_ref$package} ({op} {op_ver})`.",
+          "The version might be invalid."
+        )
       )
       stop(err)
     })
